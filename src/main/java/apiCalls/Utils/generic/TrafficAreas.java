54f4fab367a947be664e084c64f9d2f4b4abed2b
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2020-07-31 15:07:50 +0100

    added traffic areas class

---
+++ src/main/java/apiCalls/Utils/generic/TrafficAreas.java
@@ -0,0 +1,16 @@
+package apiCalls.Utils.generic;
+
+public class TrafficAreas {
+
+    public String[] trafficAreasByName = new String[]{
+            "NORTH_EAST",
+            "NORTH_WEST",
+            "MIDLANDS",
+            "EAST",
+            "WALES",
+            "WEST",
+            "LONDON",
+            "SCOTLAND",
+            "NORTHERN_IRELAND"
+    };
+}
