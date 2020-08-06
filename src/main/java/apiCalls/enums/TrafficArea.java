df815f3da2de90c7149b06aecad351714581acfe
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2020-08-06 16:50:01 +0100

    added get postcode method to traffic area enum

--- src/main/java/apiCalls/enums/TrafficArea.java
+++ src/main/java/apiCalls/enums/TrafficArea.java
@@ -1,5 +1,9 @@
 package apiCalls.enums;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
+
 public enum TrafficArea {
 
     NORTH_EAST("B"),
@@ -20,5 +24,44 @@ public enum TrafficArea {
 
     public String asString() {
         return trafficAreas;
+    }
+
+    Random random = new Random();
+    public String getPostCode() {
+        var result = switch (trafficAreas) {
+            case "B" :
+                yield "BD".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "C":
+                yield "M".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "D":
+                yield "B".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "F":
+                yield "IP".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "G":
+                yield "CF".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "H":
+                yield "OX".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "K":
+                yield "E".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "M":
+                yield  "EH".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            case "N":
+                yield "BT".concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
+            default:
+                throw new IllegalStateException("Unexpected value: " + trafficAreas);
+        };
+        return result;
+    }
+
+    public String randomString() {
+        return RandomStringUtils.randomAlphabetic(2);
+    }
+
+    public int randomTwoDigitInt() {
+        return random.nextInt(99);
+    }
+
+    public int randomOneDigitInt() {
+        return random.nextInt(9);
     }
 }
