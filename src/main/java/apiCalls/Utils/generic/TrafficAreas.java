aefb3cb943159620bb0cc6c0b71ab6dcdce4f081
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2020-08-06 15:08:38 +0100

    renamed method

--- src/main/java/apiCalls/Utils/generic/TrafficAreas.java
+++ src/main/java/apiCalls/Utils/generic/TrafficAreas.java
@@ -2,33 +2,32 @@ package apiCalls.Utils.generic;
 
 import apiCalls.enums.TrafficAreaPostCodes;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.Test;
 
 import java.util.Random;
 
 public class TrafficAreas {
 
     Random random = new Random();
-    public String getPostCodeByTrafficArea(TrafficAreaPostCodes postCodes) {
+    public String getPostCode(TrafficAreaPostCodes postCodes) {
         var result = switch (postCodes) {
             case NORTH_EAST:
-                yield TrafficAreaPostCodes.NORTH_EAST.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.NORTH_EAST.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case NORTH_WEST:
-                yield TrafficAreaPostCodes.NORTH_WEST.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.NORTH_WEST.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case MIDLANDS:
-                yield TrafficAreaPostCodes.MIDLANDS.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.MIDLANDS.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case EAST:
-                yield TrafficAreaPostCodes.EAST.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.EAST.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case WALES:
-                yield TrafficAreaPostCodes.WALES.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.WALES.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case WEST:
-                yield TrafficAreaPostCodes.WEST.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.WEST.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case LONDON:
-                yield TrafficAreaPostCodes.LONDON.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.LONDON.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case SCOTLAND:
-                yield TrafficAreaPostCodes.SCOTLAND.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.SCOTLAND.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
             case NORTHERN_IRELAND:
-                yield TrafficAreaPostCodes.NORTHERN_IRELAND.asString().concat(String.format("%s%s%s", randomInt(), randomOneDigitInt(), randomString()));
+                yield TrafficAreaPostCodes.NORTHERN_IRELAND.asString().concat(String.format("%s%s%s", randomTwoDigitInt(), randomOneDigitInt(), randomString()));
         };
         return String.valueOf(result);
     }
@@ -38,7 +37,7 @@ public class TrafficAreas {
         return RandomStringUtils.randomAlphabetic(2);
     }
 
-    public int randomInt() {
+    public int randomTwoDigitInt() {
         return random.nextInt(99);
     }
 
