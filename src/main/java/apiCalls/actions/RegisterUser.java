fdf5139a77be098f9f1fe997afee39b8d5e23c34
Wonderson Chideya <wonderson.chideya@dvsa.gov.uk>
2020-11-06 16:27:25 +0000

    added default values for registering a user

--- src/main/java/apiCalls/actions/RegisterUser.java
+++ src/main/java/apiCalls/actions/RegisterUser.java
@@ -136,7 +136,9 @@ public class RegisterUser {
         String firstName = faker.generateFirstName().concat(String.valueOf(Int.random(100, 999)));
         String lastName = faker.generateLastName().concat(String.valueOf(Int.random(100, 999)));
         String dateOfBirth = Int.random(1900, 2018) + "-" + Int.random(1, 12) + "-" + Int.random(1, 28);
+        String loginName = String.format("%s.%s%s", getForeName(), getFamilyName(), Int.random(1000,9999));
 
+        this.loginId = loginId == null ? loginName : getLoginId();
         this.title = title == null ? UserTitle.MR.asString() : getTitle();
         this.emailAddress = emailAddress == null ? "2" : getEmailAddress();
         this.foreName = foreName == null ? firstName : getForeName();
