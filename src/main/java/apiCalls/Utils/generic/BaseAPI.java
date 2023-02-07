0620974a7e5372c89e2a8ab155e50437e867796c
Wonderson Chideya <wondersonchideya@gmail.com>
2023-02-07 11:17:29 +0000

    updated dependencies

--- src/main/java/apiCalls/Utils/generic/BaseAPI.java
+++ src/main/java/apiCalls/Utils/generic/BaseAPI.java
@@ -6,6 +6,7 @@ import activesupport.system.Properties;
 import apiCalls.actions.AccessToken;
 import apiCalls.enums.UserRoles;
 import io.restassured.response.ValidatableResponse;
+import org.apache.hc.core5.http.HttpException;
 import org.dvsa.testing.lib.url.api.URL;
 import org.dvsa.testing.lib.url.utils.EnvironmentType;
 
@@ -23,27 +24,27 @@ public class BaseAPI {
         }
     }
 
-    public String adminJWT() {
+    public String adminJWT() throws HttpException {
         AccessToken accessToken = new AccessToken();
         return accessToken.getToken(Utils.config.getString("adminUser"), Utils.config.getString("adminPassword"), UserRoles.INTERNAL.asString());
     }
 
-    public String fetchApplicationInformation(String applicationNumber, String jsonPath, String defaultReturn) {
+    public String fetchApplicationInformation(String applicationNumber, String jsonPath, String defaultReturn) throws HttpException {
         String url = URL.build(env, String.format("application/%s/overview/", applicationNumber)).toString();
         return retrieveAPIData(url, jsonPath, defaultReturn);
     }
 
-    public String fetchTMApplicationInformation(String applicationNumber, String jsonPath, String defaultReturn) {
+    public String fetchTMApplicationInformation(String applicationNumber, String jsonPath, String defaultReturn) throws HttpException {
         String url = URL.build(env, String.format("transport-manager-application/%s", applicationNumber)).toString();
         return retrieveAPIData(url, jsonPath, defaultReturn);
     }
 
-    public String fetchInternalUserInformation(String userId, String jsonPath, String defaultReturn) {
+    public String fetchInternalUserInformation(String userId, String jsonPath, String defaultReturn) throws HttpException {
         String url = URL.build(env, String.format("user/internal/%s", userId)).toString();
         return retrieveAPIData(url, jsonPath, defaultReturn);
     }
 
-    public String retrieveAPIData(String url, String jsonPath, String defaultReturn) {
+    public String retrieveAPIData(String url, String jsonPath, String defaultReturn) throws HttpException {
         headers.getHeaders().put("Authorization", "Bearer " + adminJWT());
         ValidatableResponse response = RestUtils.get(url, headers.getHeaders());
         try {
